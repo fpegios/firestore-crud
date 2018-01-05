c155394575454e2237353eea445f31b40cb87e36
Fotis Pegios <fotispeg@gmail.com>
2018-01-05 22:36:17 +0100

    implement create and read-all operations

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 #  Firestore CRUD application
 
 Web app that handles firestore documents and support all the CRUD operations.
+The project is based on this tutorial http://androidkt.com/cloud-firestore-database-web/.
 
 ## Prerequisites
   - NodeJS
 
 ## Instructions
+
 Execute the following command to load dependencies:
 ```sh
 $ npm install -g gulp-cli
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,66 @@
-console.log('a log msg from main.js');
+var config = {
+    apiKey: "AIzaSyBPBsi5zfZCgY4muyswWAQC3wK7BNE6MCg",
+    authDomain: "realtime-database-exampl-f77f2.firebaseapp.com",
+    databaseURL: "https://realtime-database-exampl-f77f2.firebaseio.com",
+    projectId: "realtime-database-exampl-f77f2",
+    storageBucket: "realtime-database-exampl-f77f2.appspot.com",
+    messagingSenderId: "860473344145"
+};
+firebase.initializeApp(config);
+  
+var header = document.querySelector("#fullname");
+var inputFirstname = document.querySelector("#firstname");
+var inputLastname = document.querySelector("#lastname");
+var addButton = document.querySelector("#add");
+var loadButton = document.querySelector("#load");
+var userlist = document.querySelector("#userlist");
+
+var db = firebase.firestore();
+var usersRef = firebase.firestore().collection("users");
+var users = "";
+
+usersRef.get()
+.then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        console.log(doc.id, " => ", doc.data());
+        users = users.concat("<p id=\"" + doc.id + "\">"+doc.data().firstName + " " + doc.data().lastName + "</p>");
+    });
+    userlist.innerHTML = users;
+}).catch(function(error){
+    console.log("Got an error: ", error);
+});;
+
+// add new user
+addButton.addEventListener("click",function(){
+    var firstName = inputFirstname.value;
+    var lastName = inputLastname.value;
+
+    usersRef.add({
+        firstName: firstName,
+        lastName: lastName
+    })
+    .then(function(doc) {
+        newDocId = doc.id;
+        console.log("Document written with ID: ", doc.id);
+        users = users.concat("<p id=\"" + doc.id + "\">"+firstName + " " + lastName + "</p>");
+        userlist.innerHTML = users;
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+});
+
+// load and show users collection
+loadButton.addEventListener("click",function(){
+    users = "";
+    usersRef.get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+            users = users.concat("<p id=\"" + doc.id + "\">"+doc.data().firstName + " " + doc.data().lastName + "</p>");
+        });
+        userlist.innerHTML = users;
+    }).catch(function(error){
+        console.log("Got an error: ", error);
+    });;
+});
